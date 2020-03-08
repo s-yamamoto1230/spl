f69de7c631dc8a9ad20e2894874bff99d9678fba
yamamoto <sky.shu4230@gmail.com>
2020-03-08 19:03:57 +0900

    practice

--- practice.sql
+++ practice.sql
@@ -4,14 +4,16 @@ set names utf8;
 CREATE TABLE `users` (
     `id` INT(11),
     `name` VARCHAR(30),
-    `age` INT(3)
+    `age` INT(3),
+    primary key(id)
 );
 
 -- 部署所属ユーザ一覧テーブル作成
 CREATE TABLE `dept_users` (
     `dept_cd` VARCHAR(50),
     `dept_name` VARCHAR(50),
-    `id` INT(11)
+    `id` INT(11),
+    primary key(id)
 );
 
 -- ユーザ一覧テーブルのデータ挿入
@@ -20,6 +22,7 @@ INSERT INTO users(id,name,age) VALUES(2,'侍太郎2',23);
 INSERT INTO users(id,name,age) VALUES(3,'侍太郎3',25);
 INSERT INTO users(id,name,age) VALUES(4,'侍1',27);
 INSERT INTO users(id,name,age) VALUES(5,'侍2',21);
+INSERT INTO users(id,name,age) VALUES(6,'侍3',19);
 
 -- 部署所属ユーザ一覧テーブルのデータ挿入
 INSERT INTO dept_users(dept_cd,dept_name,id) VALUES('A0001','第一開発部',1);
